9ef5ebf3cc76ae585dd3c958af240ddfbe506f38
tonybnya <nya.tony2010@gmail.com>
2025-07-13 18:47:12 +0100

    perf+feat: add an admin user model

--- backend/models.py
+++ backend/models.py
@@ -4,6 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+class AdminUser(db.Model):
+    """Admin User model."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+
 class Vehicle(db.Model):
     """
     Vehicle model representing vehicle listings in the database.
